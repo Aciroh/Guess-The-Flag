a81e3f6e640eb37effa7bdec6266580a448b8b0a
Aciroh <horatiubolovan@gmail.com>
2022-11-18 10:56:19 +0200

    Add basic app functionality

--- guess_the_flag/guess_the_flag/ContentView.swift
+++ guess_the_flag/guess_the_flag/ContentView.swift
@@ -1,21 +1,54 @@
-//
-//  ContentView.swift
-//  guess_the_flag
-//
-//  Created by Horatiu on 18.11.2022.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var scoreTitle = ""
+    @State private var showingScore = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
--- guess_the_flag/guess_the_flag/guess_the_flagApp.swift
+++ guess_the_flag/guess_the_flag/guess_the_flagApp.swift
@@ -1,10 +1,3 @@
-//
-//  guess_the_flagApp.swift
-//  guess_the_flag
-//
-//  Created by Horatiu on 18.11.2022.
-//
-
 import SwiftUI
 
 @main
