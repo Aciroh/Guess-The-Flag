5cc4c9816a85bde9454c381ecda13e370eb873ae
Aciroh <horatiubolovan@gmail.com>
2022-11-18 10:26:51 +0200

    Create Xcode project

---
+++ guess_the_flag/guess_the_flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess_the_flag
+//
+//  Created by Horatiu on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
